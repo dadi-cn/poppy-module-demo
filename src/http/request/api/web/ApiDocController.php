43362f6e97c51524ac7f6e51894a50f25a926abb
duoli <zhaody901@126.com>
2021-02-03 14:17:45 +0800

    增加函数约定

--- src/http/request/api/web/ApiDocController.php
+++ src/http/request/api/web/ApiDocController.php
@@ -11,15 +11,14 @@ class ApiDocController extends ApiController
      * @apiVersion        1.0.0
      * @apiName           DemoApidocHow
      * @apiGroup          Demo
-     * @apiParam {bool}           boolean 布尔值类型
-     * @apiParam {int}            number 数值
-     * @apiParam {int{100-999}}   number_range 数值范围
-     * @apiParam {string}         string 字串
-     * @apiParam {string{..5}}    string_mx 字串最大5
+     * @apiParam {int}            number         数值
+     * @apiParam {int{100-999}}   number_range   数值范围
+     * @apiParam {string}         string         字串
+     * @apiParam {string{..5}}    string_mx      字串最大5
      * @apiParam {string{2..5}}   string_between 字串间隔
      * @apiParam {int{2..5}}      number_between 数值间隔
-     * @apiParam {int=1,2,3,99}   number_select 数值间隔
-     * @apiParam {string=banana,apple,ball}  string_select 字串枚举
+     * @apiParam {int=1,2,3,99}   number_select  数值间隔
+     * @apiParam {string=banana,apple,ball} string_select  字串枚举
      */
     public function how()
     {
