a095f26b2e75a4a30bc642ec871c4406137b99cd
duoli <zhaody901@126.com>
2021-03-21 00:07:27 +0800

    Add Factory 生成, 修改 system 模块文档

---
+++ src/database/factories/DemoDbFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Demo\Models\DemoDb;
+use Faker\Generator as Faker;
+
+$factory->define(DemoDb::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
