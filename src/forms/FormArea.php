b2e034bf1da8a01b41c6a3bbedca1b930eeef32a
duoli <zhaody901@126.com>
2021-04-26 16:03:08 +0800

    地区组件的使用

--- src/forms/FormArea.php
+++ src/forms/FormArea.php
@@ -2,7 +2,6 @@
 
 namespace Demo\Forms;
 
-use Poppy\Framework\Validation\Rule;
 
 class FormArea extends FormBaseWidget
 {
@@ -20,23 +19,7 @@ class FormArea extends FormBaseWidget
      */
     public function form()
     {
-        $this->textarea('content', 'Content')->rows(10)->help('文本行默认显示10行');
-        // 添加 code 代码
-        $code = <<<CODE
-\$this->textarea('content', 'Content')->rows(10)->help('文本行默认显示10行');
-CODE;
-        $this->code('content-code', 'Code@Content')->default($code);
+        call_user_func_array([$this, 'area'], ['Area']);
         $this->divider();
-
-        $this->textarea('content_required', 'Content Required')->rules([
-            Rule::required(),
-        ])->help('文本内容必须填写');
-        // 添加 code 代码
-        $code = <<<CODE
-\$this->textarea('content_required', 'Content Required')->rules([
-    Rule::required(),
-])->help('文本内容必须填写');
-CODE;
-        $this->code('content_required-code', 'Code@Content Required')->default($code);
     }
 }
